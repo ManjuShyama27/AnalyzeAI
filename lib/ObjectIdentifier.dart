48d5953beea0f93f49a6527ff7cae83c05bfd8bc
ManjuShyama Singhania <68655877+ManjuShyama27@users.noreply.github.com>
2025-04-14 18:18:20 +0530

    Update ObjectIdentifier.dart

--- lib/ObjectIdentifier.dart
+++ lib/ObjectIdentifier.dart
@@ -16,7 +16,7 @@ class ObjectIdentifier extends StatefulWidget {
 
 class _ObjectIdentifierState extends State<ObjectIdentifier> {
   bool isLoading = false;
-  String API_KEY = "AIzaSyCsbYA-HcMLJCxoOF49QccvXUx6o8eXMJk";
+  String API_KEY = "YOUR_GEMINI_API_KEY";
   dynamic response;
   File? image;
   PlatformFile? file;
